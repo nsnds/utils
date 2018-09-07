79b116c0279f32afa4e5d73cfbe446e37f3c73b1
nsnds <934143093@qq.com>
2018-09-07 20:49:44 +0800

    dom class操作方式

--- dom.js
+++ dom.js
@@ -19,3 +19,27 @@ export function prefixStyle (style) {
   if (vendor === 'standard') return style
   return vendor + style.charAt(0).toUpperCase() + style.substr(1)
 }
+
+// 添加class
+export function addClass (el, className) {
+  if (hasClass(el, className)) {
+    return
+  }
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
+}
+
+// 查询是否存在class
+export function hasClass (el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
+
+// 删除class
+export function removeClass (el, className) {
+  if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className = el.className.replace(reg, ' ')
+  }
+}
