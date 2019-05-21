27321f561df431097393231d8e8bd232868b6bfb
nsnds <934143093@qq.com>
2019-05-21 15:52:43 +0800

    Create string.js

---
+++ string.js
@@ -0,0 +1,14 @@
+/**
+ * ES6 String matchAll
+ */
+export function matchAll(str, regexp) {
+  var matches = [];
+  str.replace(regexp, function() {
+    var arr = [].slice.call(arguments, 0);
+    var extras = arr.splice(-2);
+    arr.index = extras[0];
+    arr.input = extras[1];
+    matches.push(arr);
+  });
+  return matches.length ? matches : null;
+}
